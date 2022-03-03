20c0b827bc06c74b5cd026aea1fc4ff4b0785d08
thalesbart <86331231+thalesbart@users.noreply.github.com>
2022-03-03 10:28:52 -0300

    Update frete.py

--- frete.py
+++ frete.py
@@ -37,9 +37,9 @@ from tkinter import Text, filedialog
 from tkcalendar import Calendar
 
 server = '*******' 
-database = '**********' 
-username = '*******' 
-password = '*******' 
+database = '*********' 
+username = '********' 
+password = '**********' 
 conn = pyodbc.connect('DRIVER={ODBC Driver 17 for SQL Server};SERVER='+server+';DATABASE='+database+';UID='+username+';PWD='+ password)
 cursor = conn.cursor()
 x="S"
